ff36f3dad677294e878fb7ad719f7e30e8736c8e
Nasrul <nasamin0211@gmail.com>
2026-04-07 23:38:33 +0800

    Exercise

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+/*import 'next_page.dart';*/
 
 void main() {
   runApp(MyApp());
@@ -9,128 +10,36 @@ class MyApp extends StatefulWidget {
 
   @override
   State<MyApp> createState() => _MyAppState();
+  
 }
 
 class _MyAppState extends State<MyApp> {
- 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-
-      debugShowCheckedModeBanner: false,
-      home: MainPageExt(),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: MainPageExt());
   }
 }
+
 class MainPageExt extends StatefulWidget {
   const MainPageExt({super.key});
 
   @override
-  State<MainPageExt> createState() => _MainPageState();
+  State<MainPageExt> createState() => _MainPageExtState();
 }
 
-class _MainPageState extends State<MainPageExt> {
-String buttonName = 'Click';
-  int currentIndex = 0;
-  bool _isClicked = false;
+class _MainPageExtState extends State<MainPageExt> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          foregroundColor: Color.fromARGB(255, 249, 249, 249),
-          backgroundColor: Color.fromARGB(255, 90, 155, 219),
-          title: const Text('My App'),
-        ),
-        body: Center(
-          child: currentIndex == 0 ? Container(
-            height: double.infinity,
-            width: double.infinity,
-            color: const Color.fromARGB(255, 137, 190, 152),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(255, 172, 153, 206),
-                    foregroundColor: Color.fromARGB(255, 255, 255, 255)
-                  ),
-                  onPressed: () {
-                    setState(() {
-                       buttonName = 'Whoops';
-                    });  
-                  }, 
-                  child: Text(buttonName),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (BuildContext context)=>
-                          NextPage(),
-                      ),
-                    );
-                  }, 
-                  child: const Text('Next Page'),
-                ),
-              ],  
-            ),
-          ) 
-            : GestureDetector(
-                onTap: (){
-                  setState(() {
-                  _isClicked = !_isClicked;
-                });
-              },
-              child: _isClicked ? Image.asset(
-                'images/download.jpg'
-                ) : Image.asset('images/try1.jpg'),
-              ),
-        ),
-
-        bottomNavigationBar: BottomNavigationBar(
-          /*backgroundColor: Color.fromARGB(0, 255, 255, 255),*/
-          items: const [
-            BottomNavigationBarItem(
-              label: 'Home',
-              icon: Icon(
-                Icons.home, 
-                /*color: Color.fromARGB(255, 0, 123, 255)*/
-              ),
-            ),
-            BottomNavigationBarItem(
-              label: 'Profile',
-              icon: Icon(
-                Icons.account_circle,
-                /*color: Color.fromARGB(255, 0, 123, 255),*/
-              ),
-            ),
-             BottomNavigationBarItem(
-              label: 'Menu',
-              icon: Icon(
-                Icons.menu,
-                /*color: Color.fromARGB(255, 0, 123, 255),*/
-              ),
-            ),
-          ],
-          currentIndex: currentIndex,
-          onTap: (int index){
-            setState(() {
-              currentIndex = index;
-            });
-          },
-        ),
-      );
-  }
-}
-
-class NextPage extends StatelessWidget {
-  const NextPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text('My first App'),
+        backgroundColor: Colors.pink,
+        foregroundColor: Colors.white,
+      ),
+      body: Center (
+      child: 
+       Image.asset('images/Illustration24.png'),
+      ),
     );
   }
 }
